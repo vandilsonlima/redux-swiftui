28d082909868b40292d8b3b0ed97504da0a880a8
Vandilson Lima <vandilson.lima@dextra-sw.com>
2021-04-16 21:01:16 -0300

    Initial Commit

---
+++ Redux/ReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxApp.swift
+//  Redux
+//
+//  Created by Vandilson Lima on 16/04/21.
+//
+
+import SwiftUI
+
+@main
+struct ReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
